6b4cb384f0b0f110edd463f6d9e4a52234f4ea17
ankitaborade02 <ankitaborade64@gmail.com>
2024-10-12 00:27:48 -0700

    js files

---
+++ logic.js
@@ -0,0 +1,6 @@
+function message(){
+    const name ="john";
+    const age=28;
+    return "my name is" + name + "my age is:" + age
+}
+export default message;
---
+++ main.js
@@ -0,0 +1,7 @@
+import {add, sub, mul, div} from "./mymodule.js"
+const a = 13
+const b = 10 
+console. log("addition is:" + add(a,b))
+console.log("subtraction is:" +sub(a,b))
+console.log("multiplication is:" + mul(a,b))
+console.log("division is:" +div(a,b))
